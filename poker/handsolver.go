4e88ec33ae10e5eac35ac590c96f3f755ad3d320
Brian Esch <besch@drwholdings.com>
2020-04-12 13:42:45 -0500

    Start implementing hand solver

--- poker/handsolver.go
+++ poker/handsolver.go
@@ -1,9 +1,135 @@
 package poker
 
+import (
+	"sort"
+)
+
 const (
 	handSize int32 = 5
 )
 
-func ComareHands(h1 []Cards, h2 []Cards) {
+const (
+	highCardRank int32 = iota
+	pairRank
+	twoPairRank
+	threeKindRank
+	straightRank
+	flushRank
+	fullHouseRank
+	fourKindRank
+	straightFlushRank
+)
 
+type Hand interface {
+	rank() int32
+}
+
+type StraightFlush struct {
+	sortedCards []Card
+}
+
+func (s StraightFlush) rank() int32 {
+	return straightFlushRank
+}
+
+/*
+func solveForStraightFlush(cards []Card) (StraightFlush, bool) {
+	Cant just use flush and straight solvers, consider
+	2H 3H 4H 5H 6H 4C JH
+	there are two straight solutions and two flush solutions. The solve methods
+	just find the best straight/flush, they don't list all solutions
+}
+*/
+
+type Flush struct {
+	sortedCards []Card
+}
+
+func (f Flush) rank() int32 {
+	return flushRank
+}
+
+func solveForFlush(cards []Card) *Flush {
+	suitsMap := make(map[Suit][]Card)
+	for _, card := range cards {
+		suitsMap[card.suit] = append(suitsMap[card.suit], card)
+	}
+
+	// Here we assume that there is only one flush in the hand
+	for _, cards := range suitsMap {
+		if len(cards) >= 5 {
+			sort.Slice(cards, func(i, j int) bool {
+				return cards[i].value > cards[j].value
+			})
+
+			return &Flush{sortedCards: cards[:5]}
+		}
+	}
+
+	return nil
+}
+
+type Straight struct {
+	sortedCards []Card
+}
+
+func (s Straight) rank() int32 {
+	return straightRank
+}
+
+func solveForStraight(cards []Card) *Straight {
+	sort.Slice(cards, func(i, j int) bool {
+		return cards[i].value > cards[i].value
+	})
+
+	/*
+		result := make([]Card, 5)
+		for i, card := range cards {
+			if i == 0 || lastCard.value {
+				streakStart = i
+				lastCard = &cards
+			}
+		}
+	*/
+	return nil
+}
+
+type HighCard struct {
+	// TODO this needs to be sorted cards
+	highCard Card
+}
+
+func (h HighCard) rank() int32 {
+	return highCardRank
+}
+
+func solveForHighCard(cards []Card) *HighCard {
+	var high Card
+	for i, card := range cards {
+		if i == 0 || card.value > high.value {
+			high = card
+		}
+	}
+
+	return &HighCard{highCard: high}
+}
+
+func SolveHand(cards []Card) Hand {
+	if len(cards) == 0 {
+		panic("Do not support empty hands")
+	} else if len(cards) > 7 {
+		panic("Do not support hands larger than 7. Assumptions made in solvers that hand size does not exceed 7")
+	}
+
+	flush := solveForFlush(cards)
+	if flush != nil {
+		return flush
+	}
+
+	straight := solveForStraight(cards)
+	if flush != nil {
+		return straight
+	}
+
+	return solveForHighCard(cards)
 }
--- poker/handsolver_test.go
+++ poker/handsolver_test.go
@@ -1,6 +1,136 @@
 package poker
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestPair(t *testing.T) {
+func buildCards(cards []string) []Card {
+	result := make([]Card, 0)
+	for _, card := range cards {
+		if len(card) != 2 {
+			panic("Only handle two char card shortcut")
+		}
+
+		var value CardValue
+		switch card[0] {
+		case '2':
+			value = Two
+		case '3':
+			value = Three
+		case '4':
+			value = Four
+		case '5':
+			value = Five
+		case '6':
+			value = Six
+		case '7':
+			value = Seven
+		case '8':
+			value = Eight
+		case '9':
+			value = Nine
+		case 'T':
+			value = Ten
+		case 'J':
+			value = Jack
+		case 'Q':
+			value = Queen
+		case 'K':
+			value = King
+		case 'A':
+			value = Ace
+		default:
+			panic("Unkown card calue")
+		}
+
+		var suit Suit
+		switch card[1] {
+		case 'S':
+			suit = Spades
+		case 'H':
+			suit = Hearts
+		case 'D':
+			suit = Diamonds
+		case 'C':
+			suit = Clubs
+		default:
+			panic("Unknown suit")
+		}
+
+		result = append(result, Card{suit: suit, value: value})
+	}
+
+	return result
+}
+
+func TestHighCardBasic(t *testing.T) {
+	cards := buildCards([]string{"2H", "4C", "8D", "TS", "AS", "3H", "9C"})
+	highCard := SolveHand(cards).(*HighCard)
+
+	expected := Card{suit: Spades, value: Ace}
+	if !reflect.DeepEqual(highCard.highCard, expected) {
+		t.Errorf("%+v\n != %+v\n", highCard.highCard, expected)
+	}
+
+	if highCard.rank() != highCardRank {
+		t.Error()
+	}
+}
+
+func TestFlushBasic(t *testing.T) {
+	cards := buildCards([]string{"AS", "2S", "4S", "8S", "TS"})
+	flush := SolveHand(cards).(*Flush)
+
+	expected := buildCards([]string{"AS", "TS", "8S", "4S", "2S"})
+
+	if !reflect.DeepEqual(expected, flush.sortedCards) {
+		t.Error()
+	}
+
+	if flush.rank() != flushRank {
+		t.Error()
+	}
+}
+
+func TestFlushLong(t *testing.T) {
+	cards := buildCards([]string{"AS", "2S", "4S", "8S", "TS", "3S", "9S"})
+	flush := SolveHand(cards).(*Flush)
+
+	expected := buildCards([]string{"AS", "TS", "9S", "8S", "4S"})
+	if !reflect.DeepEqual(expected, flush.sortedCards) {
+		t.Error()
+	}
+}
+
+func TestStraightBasic(t *testing.T) {
+	/*
+		cards := buildCards([]string{"2H", "3C", "6S", "TS", "QC", "4D", "5D"})
+		straight := SolveHand(cards).(*Straight)
+
+		expected := buildCards([]string{"6S", "5D", "4D", "3C", "2H"})
+		if !reflect.DeepEqual(expected, straight.sortedCards) {
+			t.Error()
+		}
+	*/
+}
+
+func TestStraightDuplicateCards(t *testing.T) {
+
+}
+
+func TestStraightLong(t *testing.T) {
+
+}
+
+func TestStraightAceLow(t *testing.T) {
+
+}
+
+func TestStraightAceHigh(t *testing.T) {
+
+}
+
+func TestStraightAceWraparound(t *testing.T) {
+
 }
